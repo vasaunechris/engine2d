4143481dcb3ed10ac101ecbabbd3d62a6aa70afc
vasaunechris <43403741+vasaunechris@users.noreply.github.com>
2022-01-13 00:33:07 +0100

    graphicInterface init

--- src/main/java/com/sochris/engine2d/App.java
+++ src/main/java/com/sochris/engine2d/App.java
@@ -1,5 +1,7 @@
 package com.sochris.engine2d;
 
+import com.sochris.engine2d.renderer.GraphicsInterface;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        new GraphicsInterface().start();
     }
 }
---
+++ src/main/java/com/sochris/engine2d/renderer/GraphicsInterface.java
@@ -0,0 +1,47 @@
+package com.sochris.engine2d.renderer;
+
+
+public class GraphicsInterface implements Runnable {
+
+    public Thread game;
+    private boolean isRunning = true;
+
+    public void start(){
+        this.game = new Thread(this,"game");
+        game.start();
+    }
+
+    public GraphicsInterface() {
+        
+    }
+
+    private void onInit(){
+        System.out.println("Initialisation Game");
+    }
+
+    private void onQuit(){
+        this.isRunning = false;
+    }
+
+    @Override
+    public void run() {
+
+        onInit();
+        while(isRunning){
+            update();
+            render();
+        }
+        onQuit();
+        
+    }
+
+    private void update(){
+        System.out.println("Updating Game");
+    }
+
+    private void render(){
+        System.out.println("rending Game");
+
+    }
+    
+}
